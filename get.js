921f99d61f8b0640a0fa0b5cca1339768529aa1a
rohanarote <girryarohan@gmail.com>
2021-11-04 00:05:46 +0530

    init

--- get.js
+++ get.js
@@ -1,14 +1,11 @@
-// COWIN API Automation -  Node JS
-// Rishish Dixit
-
-start_point("110016", "12-06-2021");
+start_point("400012", "22-07-2021");
 
 // Starting point to process COWIN API
 function start_point(pincode, date) {
 	const https = require('https'); //import https module
-
+//calendarByDistrict?district_id=395&date=21-07-2021
 	//initiate get request
-	let req = https.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=' + pincode + '&date=' + date, function (res) {
+	let req = https.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=395&date=21-07-2021', function (res) {
 		let data = '', json_data;
 
 		// Add data when response expect data fragments
@@ -19,7 +16,10 @@ function start_point(pincode, date) {
 		res.on('end', function () {
 			json_data = JSON.parse(data);
 			//pass this JS object in the search function to process 
-			search_available_capacity(json_data);
+			//console.log(json_data.centers);
+			let new_data=json_data.centers.filter(x => (x.fee_type=="Free"));
+			//console.log(new_data);
+			search_available_capacity(new_data);
 		});
 	});
 	// return error if expectatios are not met
@@ -29,39 +29,48 @@ function start_point(pincode, date) {
 }
 
 function search_available_capacity(json_data_object) {
+	
+	
 	for (var key in json_data_object) {
-		if (typeof json_data_object[key] == 'object') {
-			search_available_capacity(json_data_object[key]);
-		} else {
-			if (key == "available_capacity" && json_data_object["available_capacity"] > 0) {
-				subject = "Vaccine Availbilty - Pincode - " + json_data_object["pincode"] + " - " + json_data_object["name"];
-				console.log(subject);
-				message_to_go = json_data_object["name"] + "(District name - " + json_data_object["district_name"] +
-					") --Total Availbilty = " + json_data_object["available_capacity"] + " (Age Limit - " + json_data_object["min_age_limit"] + " )";
-				console.log(message_to_go);
-				// pass email, subject and message and call function to send email
-				send_mail("rishish.xyz@gmail.com", subject, message_to_go);
+		
+			if (typeof json_data_object[key] == 'object') {
+				
+				search_available_capacity(json_data_object[key]);
+			} else {
+				if (key == "available_capacity" && json_data_object["available_capacity"] > 0) {
+					subject = "Vaccine Availbilty - Pincode - " + json_data_object["pincode"] + " - " + json_data_object["name"];
+					console.log(subject);
+					message_to_go = json_data_object["name"] + "(District name - " + json_data_object["district_name"] +
+						") --Total Availbilty = " + json_data_object["available_capacity"] + " (Age Limit - " + json_data_object["min_age_limit"] + " )";
+					console.log(message_to_go);
+					// pass email, subject and message and call function to send email
+					send_mail("rohanarote7@gmail.com", subject, message_to_go);
 
+				}
 			}
-		}
 	}
+	
 }
-
+/*
+console.log(json_data_object.fee_type);
+	}else if(json_data_object.fee_type=="Free"){
+		console.log(json_data_object.fee_type);*/
 function send_mail(mailid, subject, message_to_go) {
 	// import nodemailer
 	// use - npm install nodemailer
 	// before import
+	/*
 	var nm = require('nodemailer');
 	var transporter = nm.createTransport({
 		service: 'gmail',
 		auth: {
-			user: 'xxxxx@gmail.com', // put gmail user name
-			pass: 'xxxxxxxx'		// put gmail password
+			user: 'girryarohan@gmail.com', // put gmail user name
+			pass: 'proxy*77#'		// put gmail password
 		}
 	});
 
 	var mailOptions = {
-		from: 'xxxxx@gmail.com',
+		from: 'girryarohan@gmail.com',
 		to: mailid,
 		subject: subject,
 		text: message_to_go
@@ -73,5 +82,5 @@ function send_mail(mailid, subject, message_to_go) {
 		} else {
 			console.log('Email sent: ' + info.response);
 		}
-	});
+	});*/
 }
